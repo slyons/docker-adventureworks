c6d1de74085410f663c1f1e8188d328b243920da
Christopher Eaton <chris.eaton@appendmedia.com>
2020-07-14 14:23:54 -0700

    Fixed incorrect database file names (for some reason MS left 2017 in the name despite it being the 2019 version). Updated build script with better skip messages.

--- build.sh
+++ build.sh
@@ -5,21 +5,21 @@ if [ ! -f adventureworks.bak ]; then
     wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O adventureworks.bak -q
     echo "Download complete."
 else
-    echo "AdventureWorks backup file already downloaded. Skipping."
+    echo "AdventureWorks OLTP backup file already downloaded. Skipping."
 fi
 if [ ! -f adventureworks-dw.bak ]; then
     echo "Downloading AdventureWorks data warehouse backup file from Microsoft..."
     wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2019.bak -O adventureworks-dw.bak -q
     echo "Download complete."
 else
-    echo "AdventureWorks backup file already downloaded. Skipping."
+    echo "AdventureWorks data warehouse backup file already downloaded. Skipping."
 fi
 if [ ! -f adventureworks-light.bak ]; then
     echo "Downloading AdventureWorks light backup file from Microsoft..."
     wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O adventureworks-light.bak -q
     echo "Download complete."
 else
-    echo "AdventureWorks backup file already downloaded. Skipping."
+    echo "AdventureWorks light backup file already downloaded. Skipping."
 fi
 echo "Building OLTP docker image."
 docker build . -t chriseaton/adventureworks:latest --build-arg BAK_FILE=./adventureworks.bak
