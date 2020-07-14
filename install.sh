50677f42f067caba4a9267e173124587bcafd0ae
Christopher Eaton <chris.eaton@appendmedia.com>
2020-07-14 14:25:40 -0700

    Remove echo.

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ echo "Running installation script..."
 #wait for the SQL Server to come up
 sleep 30s
 
-echo $SA_PASSWORD
-
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i install.sql
 echo
